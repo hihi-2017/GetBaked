16037043f9755cf553473aa98152de121f9766b9
Sarah Dueweke <sdueweke@gmail.com>
2017-05-24 16:56:04 +1200

    two tests passing, Nav component is rendering

--- test/client/app.test.js
+++ test/client/app.test.js
@@ -5,11 +5,19 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 
 import App from '../../client/components/App'
+import Nav from '../../client/components/Nav'
 
 App.prototype.componentDidMount = () => {}
 
 test.cb('<App />', (t) => {
   const wrapper = shallow(<App />)
   t.is(wrapper.contains('get baked'), true, 'displays logo link')
+  t.end()
+})
+
+
+test.cb('<Nav />', (t) => {
+  const wrapper = shallow(<Nav />)
+  t.is(wrapper.contains('Home'), true, 'displays navbar')
   t.end()
 })
